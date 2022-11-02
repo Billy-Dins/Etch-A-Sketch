58fc72012652988235187fb8b9a6f377d04a13fe
Billy <Lrdweaver@gmail.com>
2022-11-02 06:26:51 -0400

    Added grey and rainbow buttons

--- app.js
+++ app.js
@@ -1,31 +1,49 @@
 let canvas = document.querySelector('#playArea');
 let sqrBtn = document.querySelector('#sqrBtn');
-let canvasAttributes = document.querySelector('#playArea')
+let canvasAttributes = document.querySelector('#playArea');
 let canvasRange = document.querySelector("#areaRange");
-window.addEventListener('load', canvasValue);
-canvasRange.addEventListener('change', canvasValue);
+window.addEventListener('load', updateCanvas('grey'));
+canvasRange.addEventListener('change', () => {
+    updateCanvas('grey')
+});
 
-
-// This generates a square of divs on the canvas 
-// of the selected value from the slider.
-function canvasValue(){
-    let canvasValues = document.querySelector('#areaRange').value;
-    let valueSquared = (canvasValues * canvasValues);
-    let autoStr = 'auto '.repeat(canvasValues);
+function updateCanvas(updateColor){
+    let rangeValue = document.querySelector('#areaRange').value;
+    let valueSquared = (rangeValue * rangeValue);
+    let autoStr = 'auto '.repeat(rangeValue);
     canvasAttributes.setAttribute('style', `grid-template-columns:${autoStr}`)
-    updateCanvas()
-    function updateCanvas(){
-        let allDivs = document.querySelectorAll('#gridSquare')
-        allDivs.forEach(itm => {
-            itm.remove()});
-        for (let i = 0; i < valueSquared; i++) {
-            let newDiv = document.createElement('div')
-            newDiv.setAttribute('id', 'gridSquare');
-            canvas.appendChild(newDiv);
-            newDiv.addEventListener('mouseover', (e) => {
-                var x = Math.floor(Math.random() * 256);
-                var y = Math.floor(Math.random() * 256);
-                var z = Math.floor(Math.random() * 256);
-                var bgColor = "rgb(" + x + "," + y + "," + z + ")";
-                newDiv.setAttribute('style', "background-color:rgb(" + x + "," + y + "," + z + ")")
-            });}}};
+    let allDivs = document.querySelectorAll('#gridSquare')
+    allDivs.forEach(itm => {
+        itm.remove()});
+    for (let i = 0; i < valueSquared; i++) {
+        let gridSqr = document.createElement('div')
+        gridSqr.setAttribute('id', 'gridSquare');
+        canvas.appendChild(gridSqr);
+        if (updateColor == 'grey'){
+            gridSqr.addEventListener('mouseover', (e) => {
+                gridSqr.setAttribute('style', "background-color:grey")
+            });
+        } else if (updateColor == 'rainbow'){
+        gridSqr.addEventListener('mouseover', (e) => {
+            var x = Math.floor(Math.random() * 256);
+            var y = Math.floor(Math.random() * 256);
+            var z = Math.floor(Math.random() * 256);
+            gridSqr.setAttribute('style', "background-color:rgb(" + x + "," + y + "," + z + ")")
+        })
+        } else if (updateColor == 'darken'){
+            console.log('idk yet')
+        }
+    }
+};
+let grey = document.querySelector('#greyBtn')
+grey.addEventListener('click', () => {
+    updateCanvas('grey')
+});
+let rainbow = document.querySelector('#rainbowBtn')
+rainbow.addEventListener('click', () => {
+    updateCanvas('rainbow')
+});
+let darken = document.querySelector('#darkenBtn')
+darken.addEventListener('click', () => {
+    updateCanvas('darken')
+});
